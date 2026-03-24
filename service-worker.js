91b98d1edc73ee7d018c0f25a1a73b9d4bc0be06
Aparvin71 <aparvin@live.com>
2026-03-24 08:12:54 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // ===== VERSION =====
-const CACHE_VERSION = "v39.2-smart-resume";
+const CACHE_VERSION = "v39.3.1-queue-button-sticky-fix";
 const STATIC_CACHE = `static-${CACHE_VERSION}`;
 const RUNTIME_CACHE = `runtime-${CACHE_VERSION}`;
 const AUDIO_CACHE = `audio-${CACHE_VERSION}`;
