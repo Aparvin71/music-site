c01e4c1cddfac3ed0558df215015b46fb4ccd201
Aparvin71 <aparvin@live.com>
2026-04-06 14:26:59 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // ===== VERSION =====
-const CACHE_VERSION = "v41.6.2-mobile-queue-playlists";
+const CACHE_VERSION = "v41.6.3-mobile-queue-playlists";
 const STATIC_CACHE = `static-${CACHE_VERSION}`;
 const RUNTIME_CACHE = `runtime-${CACHE_VERSION}`;
 const AUDIO_CACHE = `audio-${CACHE_VERSION}`;
