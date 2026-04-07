3c75dcacba74f53d760dd1ab85d60d0ba2c0c821
Aparvin71 <aparvin@live.com>
2026-04-07 13:56:33 -0500

    Add files via upload

--- aineo-config.js
+++ aineo-config.js
@@ -12,8 +12,8 @@
   };
 
   window.AineoConfig = {
-    version: "v42.3.11",
-    assetVersion: "42.3.11",
+    version: "v42.3.12",
+    assetVersion: "42.3.12",
     defaultArtist: "Allen Parvin",
     defaultAlbum: "Singles",
     defaultCollectionLabel: "All Songs",
--- pwa-init.js
+++ pwa-init.js
@@ -1,4 +1,4 @@
-const AINEO_APP_VERSION = "v42.3.11";
+const AINEO_APP_VERSION = "v42.3.12";
 const INSTALL_DISMISSED_KEY = "aineo_install_dismissed";
 let deferredInstallPrompt = null;
 
