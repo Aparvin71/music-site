d2adaea55be00383e2d5eec71fa0a7582e03c9e2
Aparvin71 <aparvin@live.com>
2026-04-08 17:03:30 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // ===== VERSION =====
-const CACHE_VERSION = "v42.3.40-playlist-section-polish-pass";
+const CACHE_VERSION = "v42.3.41-playlist-section-polish-pass";
 const STATIC_CACHE = `static-${CACHE_VERSION}`;
 const RUNTIME_CACHE = `runtime-${CACHE_VERSION}`;
 const AUDIO_CACHE = `audio-${CACHE_VERSION}`;
