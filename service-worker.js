c981a3573ee75132918ba8bc57a75c37f6f8242a
Aparvin71 <aparvin@live.com>
2026-04-09 10:59:33 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // ===== VERSION =====
-const CACHE_VERSION = "v42.3.55-next-level-player-upgrade";
+const CACHE_VERSION = "v42.3.56-next-level-player-upgrade";
 const STATIC_CACHE = `static-${CACHE_VERSION}`;
 const RUNTIME_CACHE = `runtime-${CACHE_VERSION}`;
 const AUDIO_CACHE = `audio-${CACHE_VERSION}`;
