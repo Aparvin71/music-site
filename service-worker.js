4aad594b77fe4c3687df9082933572a6e574c06c
Aparvin71 <aparvin@live.com>
2026-04-09 17:11:59 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // ===== VERSION =====
-const CACHE_VERSION = "v42.3.69-iphone-app-feel-polish";
+const CACHE_VERSION = "v42.3.70-visibility-and-sticky-fixes";
 const STATIC_CACHE = `static-${CACHE_VERSION}`;
 const RUNTIME_CACHE = `runtime-${CACHE_VERSION}`;
 const AUDIO_CACHE = `audio-${CACHE_VERSION}`;
