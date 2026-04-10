9f1a87739157d43e060de1875380019425b5d067
Aparvin71 <aparvin@live.com>
2026-04-10 08:00:19 -0500

    Add files via upload

--- aineo-config.js
+++ aineo-config.js
@@ -12,8 +12,8 @@
   };
 
   window.AineoConfig = {
-    version: "v42.3.74",
-    assetVersion: "42.3.74",
+    version: "v42.3.75b",
+    assetVersion: "42.3.75b",
     defaultArtist: "Allen Parvin",
     defaultAlbum: "Singles",
     defaultCollectionLabel: "All Songs",
